696fea628f3a9a7d9bdb15257449fc0983827b5b
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-05-22 18:07:12 +0200

    nits

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# terraform-aws-vpc
-Comtravo's legacy Terraform AWS VPC module
+# Comtravo's legacy Terraform AWS VPC module
+# Do not use this legacy module
 
-# **Do not use this legacy module**
+## Requirements
+
+| Name | Version |
+|------|---------|
+| terraform | >= 0.12 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| aws | n/a |
+| null | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| azs | Availability zones | `list(string)` | n/a | yes |
+| cidr | CIDR | `string` | n/a | yes |
+| enable | Enable or Disable the module | `bool` | n/a | yes |
+| environment | environment | `string` | n/a | yes |
+| replication\_factor | Number of subnets, routing tables, NAT gateways | `number` | n/a | yes |
+| vpc\_name | VPC name | `string` | n/a | yes |
+| depends\_id | For inter module dependencies | `string` | `""` | no |
+| enable\_dns\_hostnames | Enable DNS hostnames | `bool` | `true` | no |
+| enable\_dns\_support | Enable DNS support | `bool` | `true` | no |
+| nat\_az\_number | Subnet number to deploy NAT gateway in | `number` | `0` | no |
+| subdomain | Subdomain name | `string` | `""` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| depends\_id | dependency ID |
+| net0ps\_zone\_id | Private hosted zone ID |
+| private\_subdomain | Private subdomain name |
+| private\_subnets | private subnets |
+| private\_zone\_id | Private hosted zone ID |
+| public\_subdomain | Public subdomain name |
+| public\_subdomain\_zone\_id | Subdomain hosted zone ID |
+| public\_subnets | public subnets |
+| subdomain\_zone\_id | Subdomain hosted zone ID |
+| vpc\_default\_sg | Default VPC security group |
+| vpc\_id | VPC ID |
+| vpc\_private\_routing\_table\_id | Private routing table ID |
+| vpc\_public\_routing\_table\_id | Public routing table ID |
+
--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,68 @@
 output "public_subnets" {
-  value = ["${aws_subnet.public.*.id}"]
+  value = [aws_subnet.public.*.id]
+  description = "public subnets"
 }
 
 output "private_subnets" {
-  value = ["${aws_subnet.private.*.id}"]
+  value = [aws_subnet.private.*.id]
+  description = "private subnets"
 }
 
 output "vpc_id" {
-  value = "${element(concat(aws_vpc.vpc.*.id, list("")), 0)}"
+  value = element(concat(aws_vpc.vpc.*.id, [""]), 0)
+  description = "VPC ID"
 }
 
 output "vpc_default_sg" {
-  value = "${element(concat(aws_default_security_group.vpc-default-sg.*.id, list("")), 0)}"
+  value = element(
+    concat(aws_default_security_group.vpc-default-sg.*.id, [""]),
+    0,
+  )
+  description = "Default VPC security group"
 }
 
 output "net0ps_zone_id" {
-  value = "${element(concat(aws_route53_zone.net0ps.*.zone_id, list("")), 0)}"
+  value = element(concat(aws_route53_zone.net0ps.*.zone_id, [""]), 0)
+  description = "Private hosted zone ID"
 }
 
 output "private_zone_id" {
-  value = "${element(concat(aws_route53_zone.net0ps.*.zone_id, list("")), 0)}"
+  value = element(concat(aws_route53_zone.net0ps.*.zone_id, [""]), 0)
+  description = "Private hosted zone ID"
 }
 
 output "subdomain_zone_id" {
-  value = "${element(concat(aws_route53_zone.subdomain.*.zone_id, list("")), 0)}"
+  value = element(concat(aws_route53_zone.subdomain.*.zone_id, [""]), 0)
+  description = "Subdomain hosted zone ID"
 }
 
 output "public_subdomain_zone_id" {
-  value = "${element(concat(aws_route53_zone.subdomain.*.zone_id, list("")), 0)}"
+  value = element(concat(aws_route53_zone.subdomain.*.zone_id, [""]), 0)
+  description = "Subdomain hosted zone ID"
 }
 
 output "public_subdomain" {
-  value = "${var.subdomain}"
+  value = var.subdomain
+  description = "Public subdomain name"
 }
 
 output "private_subdomain" {
-  value = "${element(concat(aws_route53_zone.net0ps.*.name, list("")), 0)}"
+  value = element(concat(aws_route53_zone.net0ps.*.name, [""]), 0)
+  description = "Private subdomain name"
 }
 
 output "vpc_private_routing_table_id" {
-  value = "${element(concat(aws_default_route_table.private.*.id, list("")), 0)}"
+  value = element(concat(aws_default_route_table.private.*.id, [""]), 0)
+  description = "Private routing table ID"
 }
 
 output "vpc_public_routing_table_id" {
-  value = "${element(concat(aws_route_table.public.*.id, list("")), 0)}"
+  value = element(concat(aws_route_table.public.*.id, [""]), 0)
+  description = "Public routing table ID"
 }
 
 output "depends_id" {
-  value = "${null_resource.dummy_dependency.id}"
+  value = null_resource.dummy_dependency.id
+  description = "dependency ID"
 }
+
