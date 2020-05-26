6b9b8fc62dbee93c6fee427448a40137a86b5d4b
Puneeth Nanjundaswamy <puneeth.nanjundaswamy@comtravo.com>
2020-05-26 16:19:06 +0200

    nits

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@
 * This module creates a AWS VPC and all the resources related to it.
 * This module is used to create VPCs in your AWS account. It is a complete rewrite of our internal Terraform AWS VPC module. see branch (1.x).
 *
-* ## Current features
-* **Conditionally enable / disable VPC creation.** It is helpful when for example you want to conditionally create multiple VPCs in a single environment for reasons such as VPC peering.
-* This module helps **create explicit dependencies between VPCs and VPC peering** so that there is no race condition between VPC creation and VPC peering.
-* **Create a private Route 53 hosted zone**.
-* **Conditionally create a Route 53 public hosted zone**. For example, if your master account has `foo.com` and you want bar.foo.com to be terraformed in your sub account, you could specify `subdomain = "bar.foo.com"` and setup `DNS` propogation in your master account for `bar.foo.com`
-* Configure optionally, your private and public subnet configuration by specifying the number of subnets to be created, newbits and netnum_offset. Subnetting should be handled externally to this module. See [CIDR subnetting in terraform](https://www.terraform.io/docs/configuration-0-11/interpolation.html#cidrsubnet-iprange-newbits-netnum-)
-* You can **provide external elastic ips** to the terraform module and those would be used to create the NAT gateways. (useful for retaining ***"whilelisted"*** IP addresses in case you would have to teardown the VPC for some reason)
-*
 * **Note on Terraforming elastic IPs outside of the module. The elastic IPs should be Terraformed before specifying the vpc module. So Terraform should be applied in two phases. one for EIPs and then the VPC module.**
 *
 *
